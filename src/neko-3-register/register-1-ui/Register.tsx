8ebeb26e82c126cbd0f0416f006195b0e573e902
alexviarhey <lexa.vergey@gmail.com>
2019-12-13 19:52:57 +0300

    markup registration form and validation

--- src/neko-3-register/register-1-ui/Register.tsx
+++ src/neko-3-register/register-1-ui/Register.tsx
@@ -1,15 +1,52 @@
 import React from 'react';
+import './Register.css'
+import useForm from "react-hook-form";
 
 interface RegisterProps {
 
 }
 
-const Register: React.FC<RegisterProps> = ({}) => {
+const Register: React.FC<RegisterProps> = () => {
+
+    const {register, handleSubmit, errors} = useForm({
+        mode: 'onBlur'
+    });
+
+    const onSubmit = (data: any) => {
+        console.log(data)
+    };
 
     return (
-        <div>
-            register
+        <div className='register'>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <h1>Register</h1>
+
+                    <input ref={register((
+                        {
+                            required: 'Field is required',
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                message: "Invalid email address"
+                            }
+                        }))} name='email' placeholder='example@mail.com' className={errors.email ? 'input_error' : 'input'} type="text"/>
+                    {errors.email && errors.email.message && <span className='loginError'>{errors.email.message}</span>}
+
+
+                    <input ref={register({required: true})} name='password'  placeholder='password'
+                           className={errors.password ? 'input_error' : 'input'} type="password"/>
+                    {errors.password && <span className='loginError'>Field is required</span>}
+
+
+                    <input ref={register({required: true})} name='repeatPassword' placeholder='repeatPassword'
+                           className={errors.repeatPassword ? 'input_error' : 'input'} type="password"/>
+                    {errors.repeatPassword && <span className='loginError'>Field is required</span>}
+
+
+
+                <button type='submit' className='btn'>Register</button>
+            </form>
         </div>
+
     );
 };
 
